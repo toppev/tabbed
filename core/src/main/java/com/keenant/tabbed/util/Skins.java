b9d634296a8a10eb9b19b82ac4e96df78997bdb3
toppev <44984598+toppev@users.noreply.github.com>
2021-01-23 17:36:08 +0200

    Ignore some errors if not debugging

--- core/src/main/java/com/keenant/tabbed/util/Skins.java
+++ core/src/main/java/com/keenant/tabbed/util/Skins.java
@@ -32,8 +32,10 @@ import java.util.logging.Level;
  * Some Skin utils.
  */
 public class Skins {
+
+
     public static final List<Skin> DOT_SKINS;
-    public static final ArrayListMultimap<EntityType,Skin> MOB_SKINS;
+    public static final ArrayListMultimap<EntityType, Skin> MOB_SKINS;
     public static final Skin DEFAULT_SKIN;
 
     public static final Skin DECOR_TV;
@@ -49,6 +51,8 @@ public class Skins {
 
     private static final String profileUrl = "https://sessionserver.mojang.com/session/minecraft/profile/";
     private static final LoadingCache<String, String> profileCache;
+
+    private static boolean debug;
 
     static {
         DEFAULT_SKIN = new Skin("eyJ0aW1lc3RhbXAiOjE0MTEyNjg3OTI3NjUsInByb2ZpbGVJZCI6IjNmYmVjN2RkMGE1ZjQwYmY5ZDExODg1YTU0NTA3MTEyIiwicHJvZmlsZU5hbWUiOiJsYXN0X3VzZXJuYW1lIiwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzg0N2I1Mjc5OTg0NjUxNTRhZDZjMjM4YTFlM2MyZGQzZTMyOTY1MzUyZTNhNjRmMzZlMTZhOTQwNWFiOCJ9fX0=", "u8sG8tlbmiekrfAdQjy4nXIcCfNdnUZzXSx9BE1X5K27NiUvE1dDNIeBBSPdZzQG1kHGijuokuHPdNi/KXHZkQM7OJ4aCu5JiUoOY28uz3wZhW4D+KG3dH4ei5ww2KwvjcqVL7LFKfr/ONU5Hvi7MIIty1eKpoGDYpWj3WjnbN4ye5Zo88I2ZEkP1wBw2eDDN4P3YEDYTumQndcbXFPuRRTntoGdZq3N5EBKfDZxlw4L3pgkcSLU5rWkd5UH4ZUOHAP/VaJ04mpFLsFXzzdU4xNZ5fthCwxwVBNLtHRWO26k/qcVBzvEXtKGFJmxfLGCzXScET/OjUBak/JEkkRG2m+kpmBMgFRNtjyZgQ1w08U6HHnLTiAiio3JswPlW5v56pGWRHQT5XWSkfnrXDalxtSmPnB5LmacpIImKgL8V9wLnWvBzI7SHjlyQbbgd+kUOkLlu7+717ySDEJwsFJekfuR6N/rpcYgNZYrxDwe4w57uDPlwNL6cJPfNUHV7WEbIU1pMgxsxaXe8WSvV87qLsR7H06xocl2C0JFfe2jZR4Zh3k9xzEnfCeFKBgGb4lrOWBu1eDWYgtKV67M2Y+B3W5pjuAjwAxn0waODtEn/3jKPbc/sxbPvljUCw65X+ok0UUN1eOwXV5l2EGzn05t3Yhwq19/GxARg63ISGE8CKw=");
@@ -115,8 +119,18 @@ public class Skins {
         });
     }
 
+
+    public static void setDebug(boolean debug) {
+        Skins.debug = debug;
+    }
+
+    public static boolean isDebug() {
+        return debug;
+    }
+
     /**
      * Get a skin from an entity type.
+     *
      * @param type
      * @return
      */
@@ -126,6 +140,7 @@ public class Skins {
 
     /**
      * Get a skin from an entity type and alternate form number.
+     *
      * @param type
      * @param alternateForm Used if multiple versions of this mob exist (wither skeleton = 1, ender guardian = 1)
      * @return
@@ -141,6 +156,7 @@ public class Skins {
 
     /**
      * Get a cool skin based on a chat color (grey square with a colored dot).
+     *
      * @param color
      * @return
      */
@@ -150,6 +166,7 @@ public class Skins {
 
     /**
      * Get a skin from an online player.
+     *
      * @param player
      * @return
      */
@@ -163,6 +180,7 @@ public class Skins {
 
     /**
      * Get a Minecraft user's skin.
+     *
      * @param username
      * @return
      */
@@ -176,6 +194,7 @@ public class Skins {
 
     /**
      * Get a Minecraft user's skin.
+     *
      * @param uuid
      * @return
      */
@@ -197,8 +216,16 @@ public class Skins {
         try {
             json = (JSONObject) new JSONParser().parse(profileCache.get(uuid));
         } catch (Exception e) {
-            Tabbed.log(Level.SEVERE, "Unable to fetch skin for uuid " + uuid);
-            e.printStackTrace();
+            // This happens quite often (idk why?) so we can just ignore it if debugging is not enabled
+            if (debug) {
+                Tabbed.log(Level.WARNING, "Unable to fetch skin for uuid " + uuid);
+                e.printStackTrace();
+                try{
+                    Tabbed.log(Level.WARNING, "Body: " + profileCache.get(uuid));
+                }catch (Exception e2) {
+                    Tabbed.log(Level.WARNING, "Unable to resolve body");
+                }
+            }
             return DEFAULT_SKIN;
         }
         JSONArray properties = (JSONArray) json.get("properties");
